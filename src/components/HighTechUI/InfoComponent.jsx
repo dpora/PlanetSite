1f27e2bcfe3d07bafcdf2e27d5a5263a71351989
Daniel Pora <136dpora@gmail.com>
2025-11-03 14:29:51 -0500

    Initial design for the info component, this includes importing svg design from figma and some small changes for the content. This is a rough draft and will be improved on.

--- src/components/HighTechUI/InfoComponent.jsx
+++ src/components/HighTechUI/InfoComponent.jsx
@@ -3,13 +3,13 @@ import TextType from '../Text/TextType'
 
 export const InfoComponent = ({ selectedObject = sunData }) => {
   return (
-    <div className="absolute top-15 left-15 pointer-events-auto" style={{ width: '31.44vh', height: '60vh', zIndex: 20 }}>
+    <div className="absolute top-6 left-6 pointer-events-auto" style={{ width: '31.44vh', height: '80vh', zIndex: 20 }}>
       {/* SVG Background */}
       <svg
           className="glowing-border absolute inset-0"
-          width="262" 
-          height="500" 
-          viewBox="0 0 262 500"
+          width="260" 
+          height="620" 
+          viewBox="0 0 260 620"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           style={{
@@ -20,87 +20,91 @@ export const InfoComponent = ({ selectedObject = sunData }) => {
           }}
           preserveAspectRatio="xMinYMin meet"
       >
-        <path d="M115.517 2H220.408L247 25.878L247 475L224.84 494.537H178.55L168.701 486.722H84" stroke="#69D88E" strokeWidth="3"/>
-        <path d="M98 23H30L24 28.2193V403.5" stroke="#69D88E" strokeWidth="3"/>
-        <path d="M35.4333 471.918L26 462.794L25 28.779L30.4614 24H225.581L236.504 34.427V147.386L225.581 156.509L226.581 457.581L238 467.573L226.581 478H182.89L174.45 471.918H35.4333Z" fill="#3A6D4B" fillOpacity="0.5"/>
-        <path d="M14 467V483.361L31.7106 498H167" stroke="#69D88E" strokeWidth="3"/>
-        <path d="M25 454V462.867L36 473" stroke="#69D88E" strokeWidth="3"/>
-        <path d="M215.697 20H223.134L237.017 32.6322V146.757L226.605 155.034L227.605 457.656L240 468.546L227.605 479H181" stroke="#69D88E" strokeWidth="3"/>
-        <path d="M98 19H27.3679L12.943 31.607V342L2 333.306V87.2517" stroke="#69D88E" strokeWidth="3"/>
-        <path d="M254 28V41.9766L260 46.2021V459" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M20 70L70 20H190L240 70V90L220 110L240 130V160H210L190 140.5L90 140L50 180V260L90 300H190L210 280H240V310L220 330L240 350V550L200 590H60L20 550V350L60 310L20 270V170L60 130L20 90V70Z" fill="#3A6D4B" fillOpacity="0.5" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M240 50L210 20H240V50Z" fill="#3A6D4B"/>
+        <path d="M240 560L210 590M210 20L240 50V20H210Z" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M240 570L220 590" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M240 580L230 590" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M20 285V335" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M30 295V325" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M40 305V316" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M20 105V155" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M30 115V145" stroke="#69D88E" strokeWidth="3"/>
+        <path d="M40 125V136" stroke="#69D88E" strokeWidth="3"/>
+        {/* Planet name container */}
+        <path d="M80.5 50L50 80.5L80 110H180L210 80.5L180 50H80.5Z" fill="#3A6D4B" stroke="#69D88E" strokeWidth="5"/>
+        {/* Planet description container */}
+        <path d="M50 360L80 330H180L210 360V540L190 560H70L50 540V360Z" fill="#3A6D4B" stroke="#69D88E" strokeWidth="5"/>
       </svg>
 
       {/* Content */}
-      <div className="absolute" style={{ top: '5vh', left: '0', right: '0', zIndex: 3 }}>
-        {/* Planet Image - Centered */}
-        <div style={{ textAlign: 'center', marginBottom: '3vh', paddingLeft: '9vh' }}>
+      <div className="absolute" style={{ top: '5', left: '0', right: '0', zIndex: 3 }}>
+        {/* Planet Image - Centered in top section */}
+        <div style={{ 
+          display: 'flex', 
+          justifyContent: 'center', 
+          alignItems: 'center',
+          marginTop: '18vh', 
+          transform: 'translateX(3%)'
+        }}>
           <img 
             src={selectedObject.image} 
             alt={selectedObject.name}
             style={{
-              width: '12vh',
-              height: '12vh',
+              width: '10vh',
+              height: '10vh',
               borderRadius: '50%',
               objectFit: 'cover',
-              border: '0.2vh solid #69D88E'
+              border: '0.2vh solid #69D88E',
             }}
           />
         </div>
 
-        {/* Name */}
+        {/* Name - Inside hexagonal container */}
         <div style={{ 
-          color: '#ffffff', 
+          position: 'absolute',
+          top: '7vh',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          width: '60%',
+          textAlign: 'center',
+          color: '#A2FFC1', 
           fontFamily: 'Orbitron, monospace',
-          fontSize: '1.4vh',
-          marginBottom: '2vh',
-          textAlign: 'left',
-          paddingLeft: '4vh',
-          paddingRight: '4vh'
+          fontSize: '1.8vh',
+          fontWeight: 'bold'
         }}>
-          <div style={{ color: '#69D88E', marginBottom: '0.5vh' }}>NAME:</div>
-          <div style={{ 
-            paddingLeft: '1vh',
-            wordWrap: 'break-word',
-            overflowWrap: 'break-word',
-            maxWidth: '100%'
-          }}>
-            <TextType 
-              key={`name-${selectedObject.name}`}
-              text={[selectedObject.name]}
-              typingSpeed={7}
-              pauseDuration={1500}
-              showCursor={false}
-              cursorCharacter="|"
-            />
-          </div>
+          <TextType 
+            key={`name-${selectedObject.name}`}
+            text={[selectedObject.name]}
+            typingSpeed={7}
+            pauseDuration={1500}
+            showCursor={false}
+            cursorCharacter="|"
+          />
         </div>
 
-        {/* Description */}
+        {/* Description - Inside bottom container */}
         <div style={{ 
+          position: 'absolute',
+          top: '53vh',
+          left: '8vh',
+          right: '8vh',
           color: '#ffffff', 
           fontFamily: 'Orbitron, monospace',
-          fontSize: '1.4vh',
+          fontSize: '1.3vh',
+          lineHeight: '1.8',
           textAlign: 'left',
-          paddingLeft: '4vh',
-          paddingRight: '4vh'
+          wordWrap: 'break-word',
+          overflowWrap: 'break-word'
         }}>
-          <div style={{ color: '#69D88E', marginBottom: '0.5vh' }}>DESCRIPTION:</div>
-          <div style={{ 
-            paddingLeft: '1vh', 
-            lineHeight: '1.6',
-            wordWrap: 'break-word',
-            overflowWrap: 'break-word',
-            maxWidth: '90%'
-          }}>
-            <TextType 
-              key={`desc-${selectedObject.name}`}
-              text={[selectedObject.description]}
-              typingSpeed={7}
-              pauseDuration={15}
-              showCursor={false}
-              cursorCharacter="|"
-            />
-          </div>
+          <TextType 
+            key={`desc-${selectedObject.name}`}
+            text={[selectedObject.description]}
+            typingSpeed={7}
+            pauseDuration={15}
+            showCursor={false}
+            cursorCharacter="|"
+          />
         </div>
       </div>
     </div>
